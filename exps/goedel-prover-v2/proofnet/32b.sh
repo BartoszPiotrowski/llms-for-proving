ebe6ed5614ffbbda3f909d464ac93d4093eab47d
Bartosz Piotrowski <bpio@meta.com>
2025-09-29 15:30:53 -0400

    32B

---
+++ exps/goedel-prover-v2/proofnet/32b.sh
@@ -0,0 +1,7 @@
+
+./generate-with-vllm.py \
+    --model Goedel-LM/Goedel-Prover-V2-32B \
+    --dataset data/proofnet/all-prep.jsonl \
+    --prompt_template prove \
+    --n 32 \
+    --output ${0%.*}.json
