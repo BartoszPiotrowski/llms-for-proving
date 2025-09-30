12b448e9bac17f6eb6dfae5140565c15f84c2820
Bartosz Piotrowski <bpio@meta.com>
2025-09-30 17:07:27 -0400

    .

--- exps/goedel-prover-v2/proofnet/8b.sh
+++ exps/goedel-prover-v2/proofnet/8b.sh
@@ -3,5 +3,6 @@
     --model Goedel-LM/Goedel-Prover-V2-8B \
     --dataset data/proofnet/all-prep.jsonl \
     --prompt_template prove \
-    --n 32 \
+    --n 16 \
+    --data_parallel_size 8 \
     --output ${0%.*}.json
